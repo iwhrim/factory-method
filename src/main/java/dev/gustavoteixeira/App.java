c3eee6bbc2253e6f5280ca5fec87585b5579529b
Gustavo Teixeira <iwhrim@gmail.com>
2021-03-07 01:54:19 -0300

    Skeleton

---
+++ src/main/java/dev/gustavoteixeira/App.java
@@ -0,0 +1,13 @@
+package dev.gustavoteixeira;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
